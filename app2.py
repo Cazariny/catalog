e76666fb0d6fdc2a64898d60a675b5d898b2c3ce
Cazariny <cazares.d@outlook.com>
2018-10-23 20:45:39 -0500

    first commit

---
+++ app2.py
@@ -0,0 +1,105 @@
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Categories, Items, User
+from flask import session as login_session
+import random
+import string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+@app.route('/')
+def principal():
+    categories = session.query(Categories)
+    creator = getUserInfo(categories.user_id)
+    items = session.query(Items).order_by(desc(Items.name))
+    if 'username' not in login_session or creator.id != login_session['user_id']:
+        return render_template('publicmenu.html', items=items, categories=categories, creator=creator)
+    else:
+        return render_template('menu.html', items=items, categories=categories, creator=creator)
+
+
+@app.route('/catalog/<string:categories_name>/items')
+def itemMenu(categories_name):
+    if 'username' not in login_session:
+        return redirect('/login')
+    categories= session.query(Categories)
+
+
+
+# Create a new menu item
+@app.route('/restaurant/<int:categories_id>/menu/new/', methods=['GET', 'POST'])
+def newItem(categories_id):
+    if 'username' not in login_session:
+        return redirect('/login')
+    categories = session.query(Categories).filter_by(id=categories_id).one()
+    if login_session['user_id'] != categories.user_id:
+        return "<script>function myFunction() {alert('You are not authorized to add menu items to this restaurant. Please create your own restaurant in order to add items.');}</script><body onload='myFunction()'>"
+        if request.method == 'POST':
+            newItem = Items(name=request.form['name'], description=request.form['description'], categories_id=categories_id, user_id=categories.user_id)
+            session.add(newItem)
+            session.commit()
+            flash('New Menu %s Item Successfully Created' % (newItem.name))
+            return redirect(url_for('showMenu', categories_id=categories_id))
+    else:
+        return render_template('newitem.html', categories_id=categories_id)
+
+# Edit a menu item
+
+
+@app.route('/restaurant/<int:categories_id>/menu/<int:items_id>/edit', methods=['GET', 'POST'])
+def editMenuItem(categories_id, items_id):
+    if 'username' not in login_session:
+        return redirect('/login')
+    editedItem = session.query(Items).filter_by(id=items_id).one()
+    categories = session.query(Categories).filter_by(id=categories_id).one()
+    if login_session['user_id'] != categories.user_id:
+        return "<script>function myFunction() {alert('You are not authorized to edit menu items to this restaurant. Please create your own restaurant in order to edit items.');}</script><body onload='myFunction()'>"
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        if request.form['description']:
+            editedItem.description = request.form['description']
+        session.add(editedItem)
+        session.commit()
+        flash('Menu Item Successfully Edited')
+        return redirect(url_for('showMenu', categories_id=categories_id))
+    else:
+        return render_template('editmenuitem.html', categories_id=categories_id, items=items_id, item=editedItem)
+
+
+# Delete a menu item
+@app.route('/restaurant/<int:categories_id>/menu/<int:items.id>/delete', methods=['GET', 'POST'])
+def deleteMenuItem(categories_id, items_id):
+    if 'username' not in login_session:
+        return redirect('/login')
+    categories = session.query(Categories).filter_by(id=categories_id).one()
+    itemToDelete = session.query(Items).filter_by(id=items_id).one()
+    if login_session['user_id'] != categories.user_id:
+        return "<script>function myFunction() {alert('You are not authorized to delete menu items to this restaurant. Please create your own restaurant in order to delete items.');}</script><body onload='myFunction()'>"
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+        flash('Menu Item Successfully Deleted')
+        return redirect(url_for('showMenu', categories_id=categories_id))
+    else:
+        return render_template('deleteMenuItem.html', item=itemToDelete)
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
---
+++ models.py
@@ -0,0 +1,87 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
+import random, string
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+
+Base = declarative_base()
+secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True)
+    picture = Column(String)
+    email = Column(String)
+    password_hash = Column(String(64))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(secret_key, expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(secret_key)
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            # Valid Token, but expired
+            return None
+        except BadSignature:
+            # Invalid Token
+            return None
+        user_id = data['id']
+        return user_id
+
+
+class Categories(Base):
+    __tablename__ = 'categories'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40), index=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'item_id': self.item_id
+        }
+
+
+class Items(Base):
+    __tablename__ = 'items'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    categories_id = Column(Integer, ForeignKey('categories.id'))
+    categories = relationship(Categories)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+        }
+
+
+
+engine = create_engine('sqlite:///catalog.db')
+
+Base.metadata.create_all(engine)
