bc3c362a1e67a653197664afe3004bfab03603e4
Cazariny <cazares.d@outlook.com>
2019-02-14 19:57:10 -0600

    Fix aplication

--- populate.py
+++ populate.py
@@ -1,52 +1,27 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import json
-from catalog.models import Base, Categories
-
-engine = create_engine('sqlite:///catalog.db')
-Base.metadata.bind = engine
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
-
-category_json = json.loads("""{
-"all_categories": [
-  {
-    "name": "Soccer"
-  },
-  {
-    "name": "Football"
-  },
-  {
-    "name": "Snowboard"
-  },
-  {
-    "name": "TaeKwonDo"
-  },
-  {
-    "name": "Wu-shu"
-  },
-  {
-    "name": "Swimming"
-  },
-  {
-    "name": "Basketball"
-  },
-  {
-    "name": "Baseball"
-  },
-  {
-    "name": "Skating"
-  }
-]
-}""")
+from catalog import db
+from catalog.models import Categories
 
 
-for e in category_json['all_categories']:
-    category_input = Categories(
-        name=str(e['name']),
-        user_id=1
-    )
-    session.add(category_input)
-    session.commit()
+def new_category(name):
+    """
+    new_category creates a new Category object given the name
+    """
+    category = Categories()
+    category.name = name
+    return category
 
-print "added categories"
+
+def main():
+    # list of category names
+    categories = [
+        'Soccer', 'Football', 'Snowboard',
+        'Wu-shu', 'Swimming', 'Basketball',
+        'Baseball', 'Skating']
+    for c in categories:
+        db.session.add(new_category(c))
+    # save all categories to db
+    db.session.commit()
+
+
+if __name__ == '__main__':
+    main()
